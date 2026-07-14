4b5c9c95c22abbb0503753edfb4900de086b50f7
Shravani Dakve <shravanisdakve@gmail.com>
2026-07-14 14:00:00 +0530

    add app shell with routing and global styles

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import FloatingButtons from './components/FloatingButtons'
+import MobileCallBar from './components/MobileCallBar'
+import Home from './pages/Home'
+import Services from './pages/Services'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import NotFound from './pages/NotFound'
+
+export default function App() {
+  return (
+    <div className="flex min-h-screen flex-col bg-night pb-16 text-white md:pb-0">
+      <Navbar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+      <FloatingButtons />
+      <MobileCallBar />
+    </div>
+  )
+}
